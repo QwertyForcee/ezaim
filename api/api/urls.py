fdf8c2fc26d1fa453921e0bc8f82fb77127e7f80
omwi <bsuiromwi@gmail.com>
2022-12-13 20:52:26 +0300

    add register view

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,18 +1,3 @@
-"""api URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import routers, permissions
@@ -22,7 +7,7 @@ from drf_yasg import openapi
 from api import settings
 from ezaim.views import (
     not_found, app_error,
-    login,
+    login, register,
     UserViewSet,
     UserSettingsViewSet,
     CurrencyViewSet,
@@ -57,7 +42,8 @@ router.register(r'currencies', CurrencyViewSet, basename='currencies')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
-    path('login', login),
+    path('api/v1/auth/login', login),
+    path('api/v1/auth/register', register),
 ]
 
 if settings.DEBUG:
--- api/ezaim/views.py
+++ api/ezaim/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpRequest
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from rest_framework import viewsets, mixins
+from api.settings import JWT_KEY
 import jwt
 
+# todo: delete csrf exempt, and its usages
 from django.views.decorators.csrf import csrf_exempt
 
 
 from ezaim.models import (
     User, TelegramUser, UserSettings,
     PaymentCard, Payment, Loan, 
+    PassportData, Address,
     Currency, Log
 )
 from ezaim.serializers import (
@@ -31,9 +34,6 @@ def app_error(request, *args, **kwargs):
 def not_authorized(request, *args, **kwargs):
     return JsonResponse(data={"message": "Unauthorized"}, status=401)
 
-# todo: get key from settings from dotenv
-# todo: return csrf cookies
-key = 'roflan'
 @csrf_exempt
 def login(request, *args, **kwargs):
     email, password = request.POST['email'], request.POST['password']
@@ -49,12 +49,98 @@ def login(request, *args, **kwargs):
         {
             "id": user.pk
         }, 
-        key, 
+        JWT_KEY, 
         algorithm='HS256'
     )
     return JsonResponse({
         "access_token": token
     })
+
+
+def parse_address(address) -> Address:
+    country = address.get('country', None)
+    state = address.get('state', None)
+    city = address.get('city', None) 
+    code_soato = address.get('codeSoato', None)
+    street = address.get('street', None)
+    house = address.get('house', None)
+    flat = address.get('flat', None)
+    mail_index = address.get('mailIndex', None)
+    return Address(
+        country=country,
+        state=state,
+        city=city,
+        code_soato=code_soato,
+        street=street,
+        house=house,
+        flat=flat,
+        mail_index=mail_index
+    )
+
+@csrf_exempt
+def register(request: HttpRequest, *args, **kwargs):
+    data = request.POST
+    email = data.get('email', None)
+    password = data.get('password', None)
+    phone_number = data.get('phoneNumber', None)
+    password = data.get('password', None)
+    salary = data.get('salary', None)
+
+    name = data.get('name', None)
+    surname = data.get('surname', None)
+    passport_number = data.get('passportNumber', None)
+    identification_number = data.get('identificationNumber', None)
+    issue_date = data.get('issueDate', None)
+    expiry_date = data.get('expiryDate', None)
+    authority = data.get('authority', None)
+    nationality = data.get('nationality', None)
+
+    sex = data.get('sex', None)
+    resident = data.get('resident', None)
+    birth_date = data.get('birthDate', None)
+
+    try:
+        birth_address = parse_address(data['birthAddress'])
+        registration_address = parse_address(data['registrationAddress'])
+        residential_address = parse_address(data['residentialAddress'])
+    except Exception:
+        print('something went wrong')
+
+    user = User(
+        email=email,
+        password=password,
+        phone_number=phone_number,
+        salary=salary
+    )
+    passport = PassportData(
+        user_id=user.pk,
+        name=name,
+        surname=surname,
+        passport_number=passport_number,
+        identification_number=identification_number,
+        issue_date=issue_date,
+        expiry_date=expiry_date,
+        authority=authority,
+        nationality=nationality,
+        sex=sex,
+        resident=resident,
+        birth_date=birth_date,
+        birth_place=birth_address,
+        registration_address=registration_address,
+        residential_address=residential_address
+    )
+
+    token = jwt.encode(
+        {
+            "id": user.pk
+        }, 
+        JWT_KEY, 
+        algorithm='HS256'
+    )
+    return JsonResponse({
+        "access_token": token
+    })
+    
 
 
 
