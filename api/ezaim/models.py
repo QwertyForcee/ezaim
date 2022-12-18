3abbe939b8e069fe9889d5137c2950909146dc30
omwi <bsuiromwi@gmail.com>
2022-12-18 16:25:12 +0300

    add personal data encryption

--- api/ezaim/models.py
+++ api/ezaim/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from encrypted_model_fields.fields import EncryptedCharField
 
 class BaseDbModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -30,14 +31,14 @@ class User(BaseDbModel):
         return self.email
 
 class Address(BaseDbModel):
-    country = models.CharField(max_length=255)
-    state = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    code_soato = models.CharField(max_length=255)
-    street = models.CharField(max_length=255)
-    house = models.CharField(max_length=255)
-    flat = models.CharField(max_length=255)
-    mail_index = models.CharField(max_length=255)
+    country = EncryptedCharField(max_length=255)
+    state = EncryptedCharField(max_length=255)
+    city = EncryptedCharField(max_length=255)
+    code_soato = EncryptedCharField(max_length=255)
+    street = EncryptedCharField(max_length=255)
+    house = EncryptedCharField(max_length=255)
+    flat = EncryptedCharField(max_length=255)
+    mail_index = EncryptedCharField(max_length=255)
 
     class Meta:
         verbose_name_plural = "Addresses"
@@ -51,14 +52,14 @@ class PassportData(BaseDbModel):
         on_delete=models.CASCADE,
         primary_key=True
     )
-    name = models.CharField(max_length=255)
-    surname = models.CharField(max_length=255)
-    sex = models.CharField(max_length=255)
-    resident = models.CharField(max_length=255)
-    birth_date = models.CharField(max_length=255)
-    birth_country = models.CharField(max_length=255)
-    birth_state = models.CharField(max_length=255)
-    birth_city = models.CharField(max_length=255)
+    name = EncryptedCharField(max_length=255)
+    surname = EncryptedCharField(max_length=255)
+    sex = EncryptedCharField(max_length=255)
+    resident = EncryptedCharField(max_length=255)
+    birth_date = EncryptedCharField(max_length=255)
+    birth_country = EncryptedCharField(max_length=255)
+    birth_state = EncryptedCharField(max_length=255)
+    birth_city = EncryptedCharField(max_length=255)
     registration_address = models.ForeignKey(
         Address, 
         on_delete=models.RESTRICT,
@@ -69,12 +70,12 @@ class PassportData(BaseDbModel):
         on_delete=models.RESTRICT,
         related_name='residential_address'
     )
-    nationality = models.CharField(max_length=255)
-    passport_number = models.CharField(max_length=255)
-    identification_number = models.CharField(max_length=255)
-    issue_date = models.CharField(max_length=255)
-    expiry_date = models.CharField(max_length=255)
-    authority = models.CharField(max_length=255)
+    nationality = EncryptedCharField(max_length=255)
+    passport_number = EncryptedCharField(max_length=255)
+    identification_number = EncryptedCharField(max_length=255)
+    issue_date = EncryptedCharField(max_length=255)
+    expiry_date = EncryptedCharField(max_length=255)
+    authority = EncryptedCharField(max_length=255)
 
     def __str__(self) -> str:
         return f'Passport data, user: {self.user}'
